feb9c77a2d47a5b26fcc495f31d9d82be2034910
Archan Patkar <pat@archanpatkar.com>
2020-11-07 05:51:50 +0530

    done

--- base64.py
+++ base64.py
@@ -1,3 +1,4 @@
+import sys
 import math
 from os import read
 
@@ -17,25 +18,15 @@ def itoB(n,bits):
 def totalBin(data):
     return "".join([itoB(b,8) for b in data])
 
-    # print("ch------------------")
-    # print(data)
-    # print(len(data))
-    # print(curr)
-    # print(chunks)
-    # print(len(chunks))
-    # print("ch------------------")
 def chunked(data,n,pad='0'):
     l = len(data)
     chunks = []
     curr = 0
     while curr < l:
         if curr+n > l:
-            if pad:
-                print("here")
-                chunks.append(data[curr+1:] + pad*(n-(l-curr)))
+            if pad: chunks.append(data[curr+1:] + pad*(n-(l-curr)))
             break 
-        temp = data[curr:curr+n]
-        chunks.append(temp)
+        chunks.append(data[curr:curr+n])
         curr += n
     return chunks
 
@@ -52,33 +43,28 @@ def basechars(n):
 def reversebase(n):
     if n == 43: return 62
     elif n == 47: return 63
-    elif n >= 48 and n <= 57: return n-48
+    elif n >= 48 and n <= 57: return n-48 + 52
     elif n >= 65 and n <= 90: return n-65
-    elif n >= 97 and n <= 122: return n-97
+    elif n >= 97 and n <= 122: return n-97 + 26
 
 def encode(data):
     if isinstance(data,str): data = data.encode(encoding="ascii")
-    return "".join([basechars(int(n,2)) for n in ['00' + b for b in chunked(totalBin(data),6)]])
+    return "".join([basechars(int(n,2)) for n in ['00'+b for b in chunked(totalBin(data),6)]])
 
 def decode(data):
     if isinstance(data,str): data = data.encode(encoding="ascii")
-    # print(data)
     t1 = "".join([b[2:] for b in [itoB(reversebase(n),8) for n in data]])
-    print(t1)
-    # if len(t1) % 8: t1 = t1[:len(t1)-len(t1) % 8]
-    # print(len(t1))
     return [int(c,2) for c in chunked(t1,8,False)]
 
-encoded = encode("ABC")
-print(encoded)
-
-decoded = decode(encoded)
-print(bytearray(decoded))
-
-encoded = encode(readFile("test.txt"))
-print(encoded)
-
-decoded = decode(encoded)
-writeFile("decodedtest.txt", bytearray(decoded))
-print(decoded)
-print("".join([chr(c) for c in decoded]))
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    if mode == "encode":
+        d = sys.argv[2]
+        if d == "-s": print(encode(sys.argv[3]))
+        else: print(encode(readFile(d)))
+    elif mode == "decode":
+        d = sys.argv[2]
+        if d == "-s": print("".join([chr(n) for n in decode(sys.argv[3])]))
+        else: print(decode(readFile(d)))
+    else: print("No such operation")
+    sys.exit(1)
